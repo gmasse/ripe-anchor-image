920fc5fdd75122b410311f3c05e1f39c71c24620
Germain Masse <gmasse@users.noreply.github.com>
2019-02-04 19:43:15 +0100

    Update buildvm.sh

--- buildvm.sh
+++ buildvm.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-DIR=$HOME/dib
-DIC=$DIR/env/bin/disk-image-create
-IMAGE_PATH="/data/images/centos7-ripe-anchor"
+DIR=$HOME/ripe-anchor-image
+DIR_IMAGE=$DIR/images
+IMAGE_PATH="$DIR_IMAGE/centos7-ripe-anchor"
+DIC=$HOME/dib/env/bin/disk-image-create
 
 ARCH="amd64"
-BASE_ELEMENTS="centos7 bootloader cloud-init-datasources openssh-server "
+BASE_ELEMENTS="centos7 bootloader cloud-init-disable-resizefs cloud-init-datasources dracut-regenerate posix openssh-server"
 export DIB_BOOTLOADER_DEFAULT_CMDLINE="net.ifnames=0"
 export DIB_CLOUD_INIT_DATASOURCES="OpenStack"
 export DIB_BLOCK_DEVICE_CONFIG=$(< $DIR/block-device-custom.yaml)
 
-$DIC vm $BASE_ELEMENTS -t qcow2 -o $IMAGE_PATH
+mkdir -p $DIR_IMAGE
+$DIC vm $BASE_ELEMENTS -t raw -o $IMAGE_PATH

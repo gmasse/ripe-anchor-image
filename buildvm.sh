b8091147cacfe73556eb9ff07f505894d656caf2
Germain Masse <g-dev@masse.me>
2019-02-11 01:35:25 +0100

    Fix CentOS 7 cloud-init bug with metadata service access. Regenerate Dracut to support LVM.

--- buildvm.sh
+++ buildvm.sh
@@ -4,18 +4,17 @@ DIR=$HOME/ripe-anchor-image
 DIR_IMAGE=$DIR/images
 IMAGE_PATH="$DIR_IMAGE/centos7-ripe-anchor"
 DIC=$HOME/dib/env/bin/disk-image-create
+export ELEMENTS_PATH=$DIR/elements
 
 ARCH="amd64"
-BASE_ELEMENTS="centos7 bootloader cloud-init-datasources openssh-server"
-#BASE_ELEMENTS="centos7 bootloader cloud-init-datasources openssh-server local-config devuser"
-#BASE_ELEMENTS="centos7 bootloader cloud-init-disable-resizefs cloud-init-datasources dracut-regenerate posix openssh-server"
-export DIB_BOOTLOADER_DEFAULT_CMDLINE="net.ifnames=0"
-export DIB_CLOUD_INIT_DATASOURCES="OpenStack"
-export DIB_BLOCK_DEVICE_CONFIG="file://$DIR/block-device-custom.yaml"
+#BASE_ELEMENTS="centos7 bootloader cloud-init-datasources c7-cloud-init-fix dracut-regenerate openssh-server devuser"
+BASE_ELEMENTS="centos7 bootloader cloud-init-datasources c7-cloud-init-fix dracut-regenerate openssh-server"
 #export DIB_DEV_USER_USERNAME="bob"
 #export DIB_DEV_USER_PWDLESS_SUDO="Yes"
 #export DIB_DEV_USER_PASSWORD="mypassword"
-#export DIB_LOCAL_CONFIG_USERNAME="centos"
+export DIB_BOOTLOADER_DEFAULT_CMDLINE="net.ifnames=0"
+export DIB_CLOUD_INIT_DATASOURCES="OpenStack"
+export DIB_BLOCK_DEVICE_CONFIG="file://$DIR/block-device-custom.yaml"
 
 mkdir -p $DIR_IMAGE
 $DIC vm $BASE_ELEMENTS -t qcow2 -o $IMAGE_PATH
---
+++ elements/c7-cloud-init-fix/README.rst
@@ -0,0 +1,2 @@
+Temporay fix cloud-init bug in Centos 7. To be removed when patch will be upstreamed.
+https://bugs.centos.org/view.php?id=15629
